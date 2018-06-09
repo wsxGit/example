b8011b16c2887f8695d38232c13dd7ae8285ec55
wsxGit <ashleygo17@hotmail.com>
2018-06-09 15:00:34 +0800

    修改bug

--- src/main/java/com/yyt/example/GeneratorMain.java
+++ src/main/java/com/yyt/example/GeneratorMain.java
@@ -1,6 +1,5 @@
 package com.yyt.example;
 
-import com.yyt.example.entity.AEntity;
 import com.yyt.example.entity.sys.MenuEntity;
 import com.yyt.example.generator.GeneratorUtils;
 import com.yyt.example.generator.ParseEntity;
@@ -12,7 +11,7 @@ public class GeneratorMain {
 
     public static void main(String[] args) throws Exception {
         List<ParseEntity> parseEntities = new ArrayList<>();
-        parseEntities.add(new ParseEntity(AEntity.class, "Integer", "A管理", "sys"));
-        GeneratorUtils.replaceContent(parseEntities, "e:\\project\\example\\src\\main");
+        parseEntities.add(new ParseEntity(MenuEntity.class, "Integer", "A管理", "sys"));
+        GeneratorUtils.replaceContent(parseEntities, "D:\\Demo\\example\\src\\main");
     }
 }
